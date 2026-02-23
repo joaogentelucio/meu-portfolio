763108eb445216e724338458a1c4605ab80fdbd6
João Vitor <151798489+joaogentelucio@users.noreply.github.com>
2026-02-23 12:08:36 -0300

    Ajustes de ui no projeto

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -7,12 +7,13 @@ export default function Card({ projeto }: { projeto: Projeto }) {
 
     return (
        <div className={styles.card} style={{ backgroundColor: theme.colors.bottom }}>
-            <h1>{projeto.title}</h1>
+            <h1 style={{ color: theme.colors.text }}>{projeto.title}</h1>
             <img src={projeto.srcImg} alt={projeto.title} />
-            <h3>{projeto.description}</h3>
-            <p>{projeto.technologies}</p>
-            <div className={styles.cardLinks}>
-                <a href={projeto.websiteURL} target="_blank" rel="noopener noreferrer">Website</a> |
+            <h3 style={{ color: theme.colors.primary }}>{projeto.description}</h3>
+            <p style={{ color: theme.colors.silver }}>{projeto.technologies}</p>
+            <div className={styles.cardLinks} style={{ color: theme.colors.primary}}>
+                <a href={projeto.websiteURL} target="_blank" rel="noopener noreferrer">Website</a> 
+                <span style={{ color: theme.colors.text }}>|</span>
                 <a href={projeto.codeURL} target="_blank" rel="noopener noreferrer"> Code</a>
             </div>
         </div> 
