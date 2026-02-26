c4631fb93a500f4a5f97879f335fe4983aba920f
João Vitor <151798489+joaogentelucio@users.noreply.github.com>
2026-02-26 14:02:40 -0300

    Import useScrollReveal and integrate into Home component
    Added useScrollReveal hook for scroll animations.

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,14 @@
-import { useTheme } from '@/context/ThemeContext';
 import styles from './style.module.css';
+import { useTheme } from '@/context/ThemeContext';
+import { useScrollReveal } from "@/context/useScrollReveal";
 import Section1 from '@/components/sections1';
 import Section2 from '@/components/sections2';
 import Section3 from '@/components/sections3';
 
 export default function Home() {
   const { theme } = useTheme();
+  useScrollReveal();
+  
   const scrollToSection = (id: string) => {
     const section = document.getElementById(id);
     if (section) {
